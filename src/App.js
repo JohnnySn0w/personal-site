4ffdf9445d5a58a618d77575e5e8e8ca913d277d
JohnnySn0w <michael.mahan43@gmail.com>
2018-08-15 19:08:48 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Container, Segment } from 'semantic-ui-react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Container>
+        <Segment textAlign='center' raised>
+          asdf
+        </Segment>
+      </Container>
+    );
+  }
+}
+
+export default App;
