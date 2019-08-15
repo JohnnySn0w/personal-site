3a9d9dbd5d2d0a58c27c09ae13548161062ababe
JohnnySn0w <michael.mahan43@gmail.com>
2019-08-15 16:32:39 -0500

    update code to be a little less wonky

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import MainAttraction from './components/Card';
 
 class App extends Component {
-	render() {
-		return (
-			<div className="main">
-				<MainAttraction />
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="main">
+        <MainAttraction />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,91 +2,92 @@ import React, { Component } from 'react';
 import { Card, Header, Transition, Container } from 'semantic-ui-react';
 
 export default class MainAttraction extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			visible: true,
-			visible2: true,
-			flipped: false,
-		};
-		this.handleItemClick = this.handleItemClick.bind(this);
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      visible: true,
+      visible2: true,
+      flipped: false,
+    };
+    this.handleItemClick = this.handleItemClick.bind(this);
+    this.stopPropagation = this.stopPropagation.bind(this);
+  }
 
-	handleItemClick() { 
-		const { visible, visible2 } = this.state;
-		this.setState({ 
-			visible: !visible,
-			visible2: !visible2,
-		});
-	}
+  handleItemClick() { 
+    const { visible, visible2 } = this.state;
+    this.setState({ 
+      visible: !visible,
+      visible2: !visible2,
+    });
+  }
 
-	sendEmail() {
-		window.location = 'mailto:mmahan@pm.me';
-	}
+  sendEmail() {
+    window.location = 'mailto:mmahan@pm.me';
+  }
 
-	stopPropagation(event) {
-		event.stopPropagation();
-	}
+  stopPropagation(event) {
+    event.stopPropagation();
+  }
 
-	cardContent() {
-		const { flipped } = this.state;
-		if(flipped){
-			return(
-				<Card.Content textAlign='center'>
-					<Header as="h3">
+  cardContent() {
+    const { flipped } = this.state;
+    if(flipped){
+      return(
+        <Card.Content textAlign='center'>
+          <Header>
 						Michael Mahan
-						<Header.Subheader as="h4" content="A Developer"/>
-					</Header>
-					<Container className='middle' onClick={this.stopPropagation.bind(this)}>
-						<a onClick={() => this.sendEmail()}>mmahan@pm.me</a>
-					</Container>
-					<br/><br/>
-					<Container className='middle' onClick={this.stopPropagation.bind(this)}>
-						<a href="https://github.com/JohnnySn0w">GitHub</a>
-					</Container>
-				</Card.Content>
-			);
-		}
-		return (
-			<Card.Content textAlign='center'>
-				<Header as="h3">
+            <Header.Subheader content="A Developer"/>
+          </Header>
+          <Container className='middle' onClick={this.stopPropagation}>
+            <a onClick={this.sendEmail}>mmahan@pm.me</a>
+          </Container>
+          <br/><br/>
+          <Container className='middle' onClick={this.stopPropagation}>
+            <a href="https://github.com/JohnnySn0w">GitHub</a>
+          </Container>
+        </Card.Content>
+      );
+    }
+    return (
+      <Card.Content textAlign='center'>
+        <Header>
 					Michael Mahan
-					<Header.Subheader as="h4" content="A Developer"/>
+          <Header.Subheader content="A Developer"/>
 					&nbsp;
-					<br/><br/>
+          <br/><br/>
 					&nbsp;
-				</Header>
-			</Card.Content>
-		);
-	}
-	// add a card flip noise
-	render() {
-		const { 
-			visible,
-			visible2,
-			flipped
-		} = this.state;
+        </Header>
+      </Card.Content>
+    );
+  }
+  // add a card flip noise
+  render() {
+    const { 
+      visible,
+      visible2,
+      flipped
+    } = this.state;
 
-		return (
-			<div className='silvered'>
-				<Transition
-					visible={visible}
-					animation='horizontal flip'
-					duration="600"
-					onHide={() => this.setState({ visible: !visible })}
-				>
-					<Card raised onClick={() => this.handleItemClick()}>
-						<Transition
-							visible={visible2}
-							animation="fade"
-							duration="600"
-							onHide={() => this.setState({ flipped: !flipped, visible2: !visible2})}
-						>
-							{this.cardContent()}
-						</Transition>
-					</Card>
-				</Transition>
-			</div>
-		);
-	}
+    return (
+      <div className='card'>
+        <Transition
+          visible={visible}
+          animation='horizontal flip'
+          duration="600"
+          onHide={() => this.setState({ visible: !visible })}
+        >
+          <Card raised onClick={this.handleItemClick}>
+            <Transition
+              visible={visible2}
+              animation="fade"
+              duration="600"
+              onHide={() => this.setState({ flipped: !flipped, visible2: !visible2})}
+            >
+              {this.cardContent()}
+            </Transition>
+          </Card>
+        </Transition>
+      </div>
+    );
+  }
 }
